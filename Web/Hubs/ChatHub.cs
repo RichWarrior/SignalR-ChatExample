1f9f9d6235b88392bf3397c3b0dad39cb096cf51
Ömer Faruk Şahin <faruk_thecno@hotmail.com>
2019-01-29 18:33:29 +0300

    First Commit

---
+++ Web/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+
+namespace Web.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
